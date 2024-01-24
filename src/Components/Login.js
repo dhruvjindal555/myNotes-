b17d84612b37a2cdc00304d5f4c45299b046de87
Dhruv jindal <124202030+dhruvjindal555@users.noreply.github.com>
2024-01-24 19:49:11 +0530

    secret update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,7 +5,7 @@ const Login = (props) => {
     const [credentials, setCredentials] = useState({ email: "", password: "" })
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        const response = await fetch(`https://mynotesapp-ukas.onrender.com/api/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
